d3303773a46a1dde34a0d2f57fe11e3e4c963706
Ku Bon <kubon9xhd@gmail.com>
2020-05-20 22:07:59 +0700

    Update C.S.P0030.cpp

--- C.S.P0030.cpp
+++ C.S.P0030.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// 44 loc
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 void function1(int &n){
 	do{
