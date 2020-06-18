41294bdc5d89929663bf325268a40cb16260edb8
Ku Bon <kubon9xhd@gmail.com>
2020-06-18 08:09:36 +0700

    Update SumOfDigits.cpp

--- SumOfDigits.cpp
+++ SumOfDigits.cpp
@@ -1,54 +1,28 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
 
-void input ( long long int &decimal_number){
- 	printf("Convert Decimal to Binary program\n");
-	do{
-	printf("	Enter a positive number: ");
-	scanf("%lld",&decimal_number); 
-	}while(decimal_number<=0);
-}
- void display(int *a ,int n){
- printf("Binary number: ");
-	for(int i = 0; i<n ; i++){
-		printf("%d",a[i]);
-	}
- }
-int* decToBinary(int *a, int &n, int decimal_number)
-{
-	int i=0;
-	a=(int*)calloc(n,sizeof(int*));
-	while(decimal_number>0){
-            	a=(int *)realloc(a,(n+1)*sizeof(int*));		
-		a[i]=decimal_number%2;
-		decimal_number=decimal_number/2;
-		i++;
-		n++;
-	}
-        return a;
-        
-//	free(a);
-}
-
-
-
-
-int main(){
-	while(1){
-	int key, n =1;
-	int *a;
-	long long int decimal_number;
-	input(decimal_number);
-	a = decToBinary(a,n,decimal_number);
-	display(a,n);
-	printf("\nDo you want to do another convert (Y/N): ");
-	fflush(stdin);
-	//fpurge(stdin);
-	key = getchar();
-        if(key=='N'||key=='n')
-        {
+int main(int argc, char *argv[]) {
+    char a[1000];
+    int check = 1;
+    int sum = 0;
+    printf("Moi ban nhap so");
+    fflush(stdin);
+    scanf("%[^\n]",&a);
+    for(int i = 0; i < strlen(a); i++){
+        // 48-57
+        if(int(a[i]) < 48 || int(a[i]) > 57 ){
+            check = 0;
             break;
         }
-    free(a);
-	}	
+    }
+    if(check == 0){
+        printf("Day so ban nhap vao co ky tu khong phu hop");
+    }else if(check == 1){
+        for(int i = 0; i < strlen(a); i++){
+            sum += (int)a[i]-48;
+        }
+        printf("Total :%d",sum);
+    }
+    return 0;
 }
